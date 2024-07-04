d3a68e56c14ac202eaed17b23767c9b29fb25570
kachicelestine <kachicelestine2@gmail.com>
2024-07-04 15:36:34 +0200

    feat(editor): establish responsive dual-panel editor layout with sidebar

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -3,7 +3,11 @@
 import { fabric } from "fabric";
 import { useEffect, useRef } from "react";
 
+import { Navbar } from "@/features/editor/components/navbar";
+import { Footer } from "@/features/editor/components/footer";
 import { useEditor } from "@/features/editor/hooks/use-editor";
+import { Sidebar } from "@/features/editor/components/sidebar";
+import { Toolbar } from "@/features/editor/components/toolbar";
 
 export const Editor = () => {
   const { init } = useEditor();
@@ -25,8 +29,16 @@ export const Editor = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="flex-1 h-full bg-muted" ref={containerRef}>
-        <canvas ref={canvasRef} />
+      <Navbar />
+      <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+        <Sidebar />
+        <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
+          <Toolbar />
+          <div className="flex-1 h-[calc(100%-124px)] bg-muted" ref={containerRef}>
+            <canvas ref={canvasRef} />
+          </div>
+          <Footer />
+        </main>
       </div>
     </div>
   );
