ece740c9524d833b5c51325dd4ba60c83dddd295
kachicelestine <kachicelestine2@gmail.com>
2024-07-04 18:45:16 +0200

    feat(editor): implement active tool state management and UI selection tracking

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { fabric } from "fabric";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
+import { ActiveTool } from "@/features/editor/types";
 import { Navbar } from "@/features/editor/components/navbar";
 import { Footer } from "@/features/editor/components/footer";
 import { useEditor } from "@/features/editor/hooks/use-editor";
@@ -10,6 +11,24 @@ import { Sidebar } from "@/features/editor/components/sidebar";
 import { Toolbar } from "@/features/editor/components/toolbar";
 
 export const Editor = () => {
+  const [activeTool, setActiveTool] = useState<ActiveTool>("select");
+
+  const onChangeActiveTool = useCallback((tool: ActiveTool) => {
+    if (tool === activeTool) {
+      return setActiveTool("select");
+    }
+
+    if (tool === "draw") {
+      // TODO: Enable draw mode
+    }
+
+    if (activeTool === "draw") {
+      /// TODO: Disable draw mode
+    }
+    
+    setActiveTool(tool);
+  }, [activeTool]);
+
   const { init } = useEditor();
 
   const canvasRef = useRef(null);
@@ -33,9 +52,15 @@ export const Editor = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <Navbar />
+      <Navbar
+        activeTool={activeTool}
+        onChangeActiveTool={onChangeActiveTool}
+      />
       <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
-        <Sidebar />
+        <Sidebar
+          activeTool={activeTool}
+          onChangeActiveTool={onChangeActiveTool}
+        />
         <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
           <Toolbar />
           <div className="flex-1 h-[calc(100%-124px)] bg-muted" ref={containerRef}>
