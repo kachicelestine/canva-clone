d87a75c3f4bd5e21287601924d5354cd985e68f4
kachicelestine <kachicelestine2@gmail.com>
2024-07-05 09:28:07 +0200

    feat(editor/canvas): enable inserting shapes onto canvas from shape library

--- src/features/editor/components/shape-sidebar.tsx
+++ src/features/editor/components/shape-sidebar.tsx
@@ -2,7 +2,7 @@ import { IoTriangle } from "react-icons/io5";
 import { FaDiamond } from "react-icons/fa6";
 import { FaCircle, FaSquare, FaSquareFull } from "react-icons/fa";
 
-import { ActiveTool } from "@/features/editor/types";
+import { ActiveTool, Editor } from "@/features/editor/types";
 import { ShapeTool } from "@/features/editor/components/shape-tool";
 import { ToolSidebarClose } from "@/features/editor/components/tool-sidebar-close";
 import { ToolSidebarHeader } from "@/features/editor/components/tool-sidebar-header";
@@ -11,11 +11,13 @@ import { cn } from "@/lib/utils";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface ShapeSidebarProps {
+  editor: Editor | undefined;
   activeTool: ActiveTool;
   onChangeActiveTool: (tool: ActiveTool) => void;
 };
 
 export const ShapeSidebar = ({
+  editor,
   activeTool,
   onChangeActiveTool,
 }: ShapeSidebarProps) => {
@@ -37,28 +39,28 @@ export const ShapeSidebar = ({
       <ScrollArea>
         <div className="grid grid-cols-3 gap-4 p-4">
           <ShapeTool
-            onClick={() => {}}
+            onClick={() => editor?.addCircle()}
             icon={FaCircle}
           />
           <ShapeTool
-            onClick={() => {}}
+            onClick={() => editor?.addSoftRectangle()}
             icon={FaSquare}
           />
           <ShapeTool
-            onClick={() => {}}
+            onClick={() => editor?.addRectangle()}
             icon={FaSquareFull}
           />
           <ShapeTool
-            onClick={() => {}}
+            onClick={() => editor?.addTriangle()}
             icon={IoTriangle}
           />
           <ShapeTool
-            onClick={() => {}}
+            onClick={() => editor?.addInverseTriangle()}
             icon={IoTriangle}
             iconClassName="rotate-180"
           />
           <ShapeTool
-            onClick={() => {}}
+            onClick={() => editor?.addDiamond()}
             icon={FaDiamond}
           />
         </div>
