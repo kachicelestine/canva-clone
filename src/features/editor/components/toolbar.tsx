f39f09a2b493d657a89442f7cec101238cf5735a
kachicelestine <kachicelestine2@gmail.com>
2024-07-08 02:23:24 +0200

    feat(editor): add AI-powered background removal for selected canvas images

--- src/features/editor/components/toolbar.tsx
+++ src/features/editor/components/toolbar.tsx
@@ -16,7 +16,8 @@ import {
   AlignLeft, 
   AlignCenter, 
   AlignRight,
-  Trash
+  Trash,
+  SquareSplitHorizontal
 } from "lucide-react";
 
 import { isTextType } from "@/features/editor/utils";
@@ -366,6 +367,22 @@ export const Toolbar = ({
               )}
             >
               <TbColorFilter className="size-4" />
+            </Button>
+          </Hint>
+        </div>
+      )}
+      {isImage && (
+        <div className="flex items-center h-full justify-center">
+          <Hint label="Remove background" side="bottom" sideOffset={5}>
+            <Button
+              onClick={() => onChangeActiveTool("remove-bg")}
+              size="icon"
+              variant="ghost"
+              className={cn(
+                activeTool === "remove-bg" && "bg-gray-100"
+              )}
+            >
+              <SquareSplitHorizontal className="size-4" />
             </Button>
           </Hint>
         </div>
